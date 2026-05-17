8e1f125e9811cccb28724a1a5fecf51444cf3967
jnavarro <jnavarro7@users.noreply.github.com>
2026-05-17 00:31:51 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ubuntu-24.04-panasonic-setup
  Running linux on the Panasonic CF-SV9
+
+Putting together documents and scripts to help me automate the setup of this laptop that I plan to use for development. 
+
+The panasonic CF-SV9 is a Japanese domestic market laptop, the keyboard is in japanese, it has a circular trackpad, I love the design!
+
+Hope other Let's note users find this useful. 
+
+Work in progres. . .
+
+## To do
+
+Add instructions on how to install Ubuntu 24.04 linux on the machine. 
+Instructions on how to setup the machine. 
---
+++ setup-tools.sh
@@ -0,0 +1,91 @@
+#!/bin/bash 
+
+set -ex 
+
+eda-tools() {
+    echo "Installing CAD/EDA tools"
+    # Installing KiCad 10
+    sudo add-apt-repository ppa:kicad/kicad-10.0-releases
+    sudo apt update && sudo apt install --install-recommends kicad -y && sudo apt install kicad-libraries -y
+}
+
+container-tools() {
+    sudo apt install podman -y
+}
+
+system-monitoring() {
+    echo "Installing system monitoring tools"
+    sudo apt install htop btm -y
+}
+
+browsers() {
+    echo "Installing internet browsers"
+    sudo snap install brave
+    sudo snap install chromium
+}
+
+dev-tools() {
+    echo "Installing development tools"
+    sudo apt install build-essential vim -y
+    sudo snap install code --classic -y
+    wget -qO - https://mirror.mwt.me/shiftkey-desktop/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/mwt-desktop.gpg > /dev/null
+    sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/mwt-desktop.gpg] https://mirror.mwt.me/shiftkey-desktop/deb/ any main" > /etc/apt/sources.list.d/mwt-desktop.list'
+    sudo apt update && sudo apt install github-desktop
+}
+
+terminal-emulators() {
+    sudo apt update && sudo apt install terminator -y
+}
+
+udpate() {
+    sudo apt update
+}
+
+upgrade() {
+    sudo apt upgrade -y
+}
+
+autoremove() {
+    sudo apt autoremove -y
+}
+
+synergy() {
+    echo "Placeholder to install Synergy 3"
+}
+
+video-tools() {
+    echo "Installing ffmpeg used to test the facial recognition camera to use with howdy"
+    sudo apt install ffmpeg -y
+}
+
+hardware() {
+    echo "Installing howdy to use the facial recognition"
+    sudo add-apt-repository ppa:ubuntuhandbook1/howdy
+    sudo apt install howdy v4l-utils -y
+    v4l2-ctl --list-devices
+    # sudo howdy config
+    # sudo howdy add
+    # sudo nano /etc/pam.d/gdm-password
+
+}
+
+# Main 
+update
+upgrade
+terminal-emulators
+system-monitoring
+browsers
+eda-tools
+container-tools
+dev-tools
+video-tools
+# synergy
+video-tools
+hardware
+update
+upgrade
+autoremove
+#sudo apt install ./code_1.117.0-1776814346_amd64.deb 
+#sudo apt install /tmp/code_1.117.0-1776814346_amd64.deb 
+
+
