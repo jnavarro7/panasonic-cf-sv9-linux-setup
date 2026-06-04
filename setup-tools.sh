27245db6bca82e5577a539aa73eb4d744aa541e5
jnavarro <jnavarro7@users.noreply.github.com>
2026-06-04 09:57:52 -0600

    Update setup-tools.sh

--- setup-tools.sh
+++ setup-tools.sh
@@ -26,8 +26,8 @@ browsers() {
 
 dev-tools() {
     echo "Installing development tools"
-    sudo apt install build-essential vim -y
-    sudo snap install code --classic -y
+    sudo apt install build-essential vim
+    sudo snap install code --classic
     wget -qO - https://mirror.mwt.me/shiftkey-desktop/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/mwt-desktop.gpg > /dev/null
     sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/mwt-desktop.gpg] https://mirror.mwt.me/shiftkey-desktop/deb/ any main" > /etc/apt/sources.list.d/mwt-desktop.list'
     sudo apt update && sudo apt install github-desktop
